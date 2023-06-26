4201fd718854e4302a3e601e6defeb95d7fdbf4d
Sabya <sabya7011@gmail.com>
2023-06-26 15:24:35 +0530

    created factory contract

---
+++ contracts/Factory.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./EscrowDemo.sol";
+
+contract FactoryContact {
+    event ContractCreated(address newContract, uint256 timestamp);
+
+    EscrowDemo[] public deployedContracts;
+
+    function createContract() public {
+        EscrowDemo newContract = new EscrowDemo();
+        deployedContracts.push(newContract);
+        emit ContractCreated(address(newContract), block.timestamp);
+    }
+
+    function getDeployedContracts() public view returns (EscrowDemo[] memory) {
+        return deployedContracts;
+    }
+}
